61934e2d96cc3ff68965b80b74a5587642f7846b
andresflopaez1255 <andresflopaez@gmail.com>
2025-11-11 14:24:03 -0500

    fix

--- src/hooks/useTimelineHook.tsx
+++ src/hooks/useTimelineHook.tsx
@@ -2,27 +2,24 @@ import { collection } from "firebase/firestore";
 import { useFirestore, useFirestoreCollectionData } from "reactfire";
 
 export default function useTimelineHook() {
-    let sortedData
   const { status, data } = useFirestoreCollectionData(
     collection(useFirestore(), "work-expierence")
   );
 
- 
-  const cleanData = data.map(({ NO_ID_FIELD, ...rest }) => rest);
+  // si aún no hay datos, usar array vacío
+  const cleanData = (data ?? []).map(({ NO_ID_FIELD, ...rest }) => rest);
 
   function getEndDate(title: string): number {
     const match = title.match(/(\d{2})\/(\d{4})\s*-\s*(\d{2})\/(\d{4})/);
-    if (!match) return 0; // si no hay coincidencia, devolver 0 (muy antiguo)
+    if (!match) return 0;
     const [, , , month, year] = match;
-    return new Date(`${year}-${month}-01`).getTime(); // devolvemos número
+    return new Date(`${year}-${month}-01`).getTime();
   }
 
-  
-  if(status === 'success') {
-     sortedData = [...cleanData].sort(
-    (a, b) => getEndDate(b.title) - getEndDate(a.title)
-  );
-  }
+  const sortedData =
+    status === "success"
+      ? [...cleanData].sort((a, b) => getEndDate(b.title) - getEndDate(a.title))
+      : [];
 
   return {
     data: sortedData,
